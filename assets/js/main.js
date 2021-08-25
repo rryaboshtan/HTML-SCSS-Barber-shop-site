274bba94bf99e6f5eedacef690430a1a1ba8f5ac
rryaboshtan <rrvworkroman@gmail.com>
2021-08-25 23:27:48 +0300

    Updates

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+const menuBtn = document.querySelector('.menuBtn')
+const nav = document.querySelector('nav')
+const menu = document.getElementById('menu')
+nav.style.right = "-250px";
+
+const showHideNav = () => {
+    if (nav.style.right === '-250px') {
+        nav.style.right = '0'
+        menu.src = 'assets/img/close.png'
+    }
+    else {
+        nav.style.right = '-250px'
+        menu.src = 'assets/img/menu.png'
+    }
+}
+
+menuBtn.addEventListener("click", showHideNav);
+// menuBtn.addEventListener('mouseenter', showHideNav)
+
+const myLazy = new LazyLoad({ elements_selector: ".photo" });
