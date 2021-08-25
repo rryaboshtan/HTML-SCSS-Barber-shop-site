4172bdcc15b084f569ba9df82e169a44883b6c66
rryaboshtan <rrvworkroman@gmail.com>
2021-08-25 23:49:41 +0300

    Updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,17 @@
-const menuBtn = document.querySelector('.menuBtn')
-const nav = document.querySelector('nav')
-const menu = document.getElementById('menu')
+const menuBtn = document.querySelector(".menuBtn");
+const nav = document.querySelector("nav");
+const menu = document.getElementById("menu");
 nav.style.right = "-250px";
 
 const showHideNav = () => {
-    if (nav.style.right === '-250px') {
-        nav.style.right = '0'
-        menu.src = 'assets/img/close.png'
-    }
-    else {
-        nav.style.right = '-250px'
-        menu.src = 'assets/img/menu.png'
-    }
-}
+   if (nav.style.right === "-250px") {
+      nav.style.right = "0";
+      menu.src = "assets/img/close.webp";
+   } else {
+      nav.style.right = "-250px";
+      menu.src = "assets/img/menu.webp";
+   }
+};
 
 menuBtn.addEventListener("click", showHideNav);
 // menuBtn.addEventListener('mouseenter', showHideNav)
